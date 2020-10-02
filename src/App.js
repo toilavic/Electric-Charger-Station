6fec23850c78331edc8533a0e8a65fac7f22051b
Vic <t18042k@gmail.com>
2020-10-02 11:59:57 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactMapGL, {Marker} from "react-map-gl";
+import * as data from './data/chargers.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [viewport,setViewport] = useState({
+    latitude: 65.009272,
+    longitude : 25.519993,
+    zoom: 11,
+    width: '100vw',
+    height: '100vh'  
+  });
+
+  const [data,setData] = useState({hits: []});
+  // fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&latitude=65.009272&longitude=25.519993&maxresults=30") 
+  // .then(response => response.json()) 
+  // .then(data =>  {
+  //   console.log(data)
+  //   // setData(data)
+  // });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&latitude=65.009272&longitude=25.519993&maxresults=30',
+      );
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, []);
+  console.log(data)
+
+    return (
+      <div>
+        <ReactMapGL {...viewport} mapboxApiAccessToken="pk.eyJ1IjoidG9pbGF2aWMiLCJhIjoiY2tmamN5aTNsMGNoMjMzbXB4cWM4MjdtcyJ9.OVmr_4vFr40bm1pVWqhpZQ"
+                                  onViewportChange={(viewport) => {setViewport(viewport)}}
+                                  mapStyle="mapbox://styles/toilavic/ckfjda0ux0mct19nwm0cw9cqb"
+        >Marker
+        {5+5}
+        {data.DataProvider}
+        </ReactMapGL>
+      </div>
+    );
 }
-
-export default App;
