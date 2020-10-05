d8414330c4991d467a90484bc21dd5c2ae15f5fd
Vic <t18042k@gmail.com>
2020-10-05 09:23:17 +0300

    mappoint created

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import ReactMapGL, {Marker} from "react-map-gl";
-import * as data from './data/chargers.json'
+import data from './data/test.json';
 
-export default function App() {
-  const [viewport,setViewport] = useState({
-    latitude: 65.009272,
-    longitude : 25.519993,
-    zoom: 11,
-    width: '100vw',
-    height: '100vh'  
-  });
-
-  const [data,setData] = useState({hits: []});
-  // fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&latitude=65.009272&longitude=25.519993&maxresults=30") 
-  // .then(response => response.json()) 
-  // .then(data =>  {
-  //   console.log(data)
-  //   // setData(data)
-  // });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&latitude=65.009272&longitude=25.519993&maxresults=30',
-      );
-      setData(result.data);
-    };
- 
-    fetchData();
-  }, []);
-  console.log(data)
-
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 65.009272,
+        longitude : 25.519993,
+        zoom: 11,
+        width: '100vw',
+        height: '100vh'
+      },
+      data: data
+    }
+  }
+  
+  // componentDidMount() {
+  //   axios.get("https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&latitude=65.009272&longitude=25.519993&maxresults=30")
+  //     .then((result)=> {
+  //       this.setState({
+  //         data: result.data
+  //       })
+  //     })
+  // }
+  
+  render() {
+    console.log(this.state.data.data)
     return (
       <div>
-        <ReactMapGL {...viewport} mapboxApiAccessToken="pk.eyJ1IjoidG9pbGF2aWMiLCJhIjoiY2tmamN5aTNsMGNoMjMzbXB4cWM4MjdtcyJ9.OVmr_4vFr40bm1pVWqhpZQ"
-                                  onViewportChange={(viewport) => {setViewport(viewport)}}
+        <ReactMapGL {...this.state.viewport} mapboxApiAccessToken="pk.eyJ1IjoidG9pbGF2aWMiLCJhIjoiY2tmamN5aTNsMGNoMjMzbXB4cWM4MjdtcyJ9.OVmr_4vFr40bm1pVWqhpZQ"
+                                  onViewportChange={(viewport) => {this.setState({viewport: viewport})}}
                                   mapStyle="mapbox://styles/toilavic/ckfjda0ux0mct19nwm0cw9cqb"
-        >Marker
-        {5+5}
-        {data.DataProvider}
+        >
+
+        { this.state.data.data.map(e => (
+         
+         <Marker
+            key={e.id}
+            latitude={e.Latitude}
+            longitude = {e.Longitude}
+         >
+           <div>
+             {e.chargerName}
+           </div>
+        
+        </Marker>
+          
+        )) }
         </ReactMapGL>
       </div>
     );
+  }
+    
 }
